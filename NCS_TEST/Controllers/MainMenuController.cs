ec4f3a48c40ea3954face3b03507388465339bab
t-nakao001 <t-nakao@s-nishitsu.co.jp>
2026-07-22 09:59:24 +0900

    aaa

--- NCS_TEST/Controllers/MainMenuController.cs
+++ NCS_TEST/Controllers/MainMenuController.cs
@@ -5,43 +5,17 @@ namespace NCS_TEST.Controllers
 {
     public class MainMenuController : Controller
     {
-        /// <summary>
-        /// メインメニュー初期表示
-        /// </summary>
         public ActionResult Index()
         {
-            // 仮ログイン情報
-            string userName = "admin";
-            string roleName = "管理者";
+            var model = new MainMenuModel();
 
-            var model = new MainMenuViewModel
+            model.Form = new MainMenuForm()
             {
-                UserName = userName,
-                MaintenanceEnabled = (roleName == "管理者")
+                UserName = "テストユーザ",
+                MaintenanceEnabled = true
             };
 
             return View(model);
-        }
-
-        public ActionResult ConstructionList()
-        {
-            return RedirectToAction("Index", "Construction");
-        }
-
-        public ActionResult CameraList()
-        {
-            return RedirectToAction("Index", "Camera");
-        }
-
-        public ActionResult MaintenanceMenu()
-        {
-            return RedirectToAction("Index", "Maintenance");
-        }
-
-        public ActionResult Logout()
-        {
-            Session.Clear();
-            return RedirectToAction("Index", "Default");
         }
     }
 }
--- NCS_TEST/Models/MainMenuViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NCS_TEST.Models
-{
-    public class MainMenuViewModel
-    {
-        public string UserName { get; set; }
-
-        public bool MaintenanceEnabled { get; set; }
-    }
-}
