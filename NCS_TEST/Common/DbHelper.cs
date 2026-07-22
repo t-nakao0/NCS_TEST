9be263edf785e4434bd17ae15ee5099d73396156
t-nakao001 <t-nakao@s-nishitsu.co.jp>
2026-07-22 15:51:51 +0900

    2

---
+++ NCS_TEST/Common/DbHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace NCS_TEST.Common
+{
+    public class DbHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                ConfigurationManager
+                    .ConnectionStrings["NcsDb"]
+                    .ConnectionString;
+
+            return new SqlConnection(
+                connectionString);
+        }
+    }
+}
---
+++ NCS_TEST/Controllers/controllers/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using NCS_TEST.Models;
+
+namespace NCS_TEST.Controllers
+{
+    public class MainMenuController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new MainMenuModel();
+
+            model.Form = new MainMenuForm()
+            {
+                UserName = "テストユーザ",
+                MaintenanceEnabled = true
+            };
+
+            return View(model);
+        }
+    }
+}
